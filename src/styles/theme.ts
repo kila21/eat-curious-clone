f5dd210a802c6655be52d9a44ec4c98eb4e3e3c3
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-19 03:41:43 +0400

    add some colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,13 @@
 export const theme = {
   colors: {
-    primary: "",
-    secondary: "",
+    pink: "#ff73b5",
+    black: "#042f1a",
+    white: "#fff6f6",
+    grey: "#d8d8d8",
+    blue: "#80bdea",
+    green: "#1abe71",
+    darkGreen: "#0a3f3d",
+    lightGreen: "#d4ed6d",
+    red: "#f95b77",
   },
 };

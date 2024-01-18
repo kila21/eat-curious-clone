9eba7ec56aa831abc259089200a4a61d60cfb09b
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-19 02:46:07 +0400

    global styles and fonts

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    #root {
+        width: 100vw;
+        height: 100vh;
+        overflow-x: hidden;
+        font-family: 'Rubik', sans-serif;
+    }
+
+    h1,
+    h2,
+    h3 {
+        font-family: Helvetica, sans-serif;
+    }
+`;

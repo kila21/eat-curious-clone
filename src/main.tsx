5d98f78006a6c176dcc12ba30595f37e2e198415
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-19 02:56:03 +0400

    create theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { GlobalStyles } from "./styles/GlobalStyles.tsx";
+import { GlobalStyles } from "./styles/GlobalStyles.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

4138be1bd4745be628aa014fb0fc502c8fdcbef8
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-26 15:59:14 +0400

    home page products comp

---
+++ src/components/homePage/HomePageFarm.tsx
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+
+export const HomePageFarm = () => {
+  return (
+    <HomePageFarmStyled>
+      <div className="farm-content-container">
+        <FarmTitle>We're growing more than plants...</FarmTitle>
+        <FarmText>
+          We’re here to grow a positive, curious and thoughtful attitude to
+          plant-based food. Which is why we put curiosity at the heart of
+          everything we do. So much so, we’ve built our own vertical farm – a
+          near-off-grid, plant playground where we’re researching how to add
+          even more flavour, nutrition and sustainability to the way we all eat.
+        </FarmText>
+        <FarmButton>Visit Syan Farms</FarmButton>
+      </div>
+      <FarmImage
+        src="https://eat-curious.com/wp-content/uploads/2023/07/home-54-1024x576.jpg"
+        alt="farm"
+      />
+    </HomePageFarmStyled>
+  );
+};
+
+const HomePageFarmStyled = styled.div`
+  z-index: 1;
+  width: 100%;
+  display: flex;
+  color: ${(props) => props.theme.colors.white};
+  text-align: center;
+
+  & > .farm-content-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (min-width: 1025px) {
+    position: relative;
+    & > .farm-content-container {
+      width: 30%;
+      position: absolute;
+      right: 7%;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    align-items: center;
+    padding: 0 50px;
+  }
+`;
+
+const FarmTitle = styled.h4`
+  font-family: "ObviouslyNarrow", Helvetica, sans-serif;
+  font-weight: 800;
+  font-size: 40px;
+
+  @media (min-width: 1440px) {
+    font-size: 70px;
+  }
+`;
+
+const FarmText = styled.p`
+  margin-top: 30px;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 1.6;
+
+  @media (min-width: 1440px) {
+    font-size: 30px;
+  }
+`;
+
+const FarmButton = styled.div`
+  position: relative;
+  width: 150px;
+  height: 48px;
+  background-color: ${(props) => props.theme.colors.pink};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme.colors.black};
+  font-weight: 700;
+  text-transform: uppercase;
+  font-family: "ObviouslyNarrow", Helvetica, sans-serif;
+  font-size: 20px;
+  margin-top: 50px;
+`;
+
+const FarmImage = styled.img`
+  width: 100%;
+  border-radius: 15px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  @media (min-width: 1025px) {
+    width: 40%;
+    position: absolute;
+    left: 5%;
+
+    transform: translateY(50%);
+  }
+`;
---
+++ src/components/homePage/HomePageProducts.tsx
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+import { Rolling } from "../../svg-components/Rolling";
+import { HomePageProductsCard } from "./HomePageProductsCard";
+import products from "../../data/products.json";
+import { HomePageFarm } from "./HomePageFarm";
+
+export const HomePageProducts = () => {
+  return (
+    <HomePageProductsStyled>
+      <HiddenContainer>
+        <Rolling />
+      </HiddenContainer>
+
+      <div className="products-cont">
+        {products &&
+          products.map((item) => (
+            <HomePageProductsCard
+              key={`card ${item.index}, ${item.name}`}
+              index={item.index}
+              name={item.name}
+              backgroundColor={item.backgroundColor}
+              img={item.img}
+            />
+          ))}
+      </div>
+      <ProductsButton type="submit">View all</ProductsButton>
+      <HomePageFarm />
+    </HomePageProductsStyled>
+  );
+};
+
+const HomePageProductsStyled = styled.div`
+  /* background-color: ${(props) => props.theme.colors.black}; */
+
+  width: 100%;
+  min-height: 300px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  > .products-cont {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    z-index: 1;
+  }
+`;
+
+const HiddenContainer = styled.div`
+  z-index: 0;
+  background-color: ${(props) => props.theme.colors.white};
+  width: 100%;
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    height: 200px;
+    width: 150vw;
+    background-color: ${(props) => props.theme.colors.black};
+    border-radius: 150%;
+    padding-top: 138%;
+  }
+
+  & > svg {
+    z-index: 100;
+    width: 100%;
+    height: auto;
+    position: relative;
+    padding: 50px 30px;
+    & > path {
+      fill: white;
+    }
+  }
+`;
+
+const ProductsButton = styled.button`
+  z-index: 1;
+  width: 120px;
+  height: 48px;
+  font-size: 24px;
+  font-weight: 700;
+  text-transform: uppercase;
+  /* padding: 0 5px; */
+  margin-bottom: 50px;
+  border: 1px solid black;
+  background-color: ${(props) => props.theme.colors.pink};
+  border-radius: 5px;
+  cursor: pointer;
+  letter-spacing: -1px;
+`;
